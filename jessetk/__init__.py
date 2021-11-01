756839fd438800996b7f25a5ee7fc5df027e81b5
ysdede <yunusseyhandede@gmail.com>
2021-11-01 21:09:22 +0300

    Prettier clear_console method

--- jessetk/__init__.py
+++ jessetk/__init__.py
@@ -27,7 +27,9 @@ sys.path.insert(0, os.getcwd())
 ls = os.listdir('.')
 is_jesse_project = 'strategies' in ls and 'config.py' in ls and 'storage' in ls and 'routes.py' in ls
 
-clear_console = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+def clear_console(): return os.system(
+    'cls' if os.name in ('nt', 'dos') else 'clear')
 
 
 def inject_local_config() -> None:
@@ -45,12 +47,15 @@ def inject_local_routes() -> None:
     """
     local_router = locate('routes')
     from jesse.routes import router
-    print('tk ########################')
-    my_routes = [('Binance Futures', "AAVE-USDT", '5m', 'OttBands5minFixed2', 'XGaf\\')]
-    print(my_routes)
-    print('tk ########################')
-    router.set_routes(my_routes)
+
+    router.set_routes(local_router.routes)
     router.set_extra_candles(local_router.extra_candles)
+
+
+# inject local files
+if is_jesse_project:
+    inject_local_config()
+    inject_local_routes()
 
 
 def validate_cwd() -> None:
@@ -107,7 +112,8 @@ def pick(dna_log_file, sort_criteria, len1, len2) -> None:
 
     from jessetk.picker import picker
 
-    dna_picker = picker(dna_log_file, strategy, StrategyClass, len1, len2, sort_criteria)
+    dna_picker = picker(dna_log_file, strategy,
+                        StrategyClass, len1, len2, sort_criteria)
 
     dna_picker.sortdnas()
     dna_picker.create_output_file()
@@ -119,7 +125,7 @@ def pick(dna_log_file, sort_criteria, len1, len2) -> None:
 @click.argument('start_date', required=True, type=str)
 @click.argument('finish_date', required=True, type=str)
 @click.argument('eliminate', required=False, type=bool)
-def refine(dna_file, start_date: str, finish_date: str, eliminate:bool) -> None:
+def refine(dna_file, start_date: str, finish_date: str, eliminate: bool) -> None:
     """
     backtest all candidate dnas. Enter in "YYYY-MM-DD" "YYYY-MM-DD"
     """
@@ -134,6 +140,134 @@ def refine(dna_file, start_date: str, finish_date: str, eliminate:bool) -> None:
     from jessetk.refine import refine
     r = refine(dna_file, start_date, finish_date, eliminate)
     r.run(dna_file, start_date, finish_date)
+
+
+@cli.command()
+@click.argument('dna_file', required=True, type=str)
+@click.argument('start_date', required=True, type=str)
+@click.argument('finish_date', required=True, type=str)
+@click.argument('eliminate', required=False, type=bool)
+def refineth(dna_file, start_date: str, finish_date: str, eliminate: bool) -> None:
+    """
+    backtest all candidate dnas. Enter in "YYYY-MM-DD" "YYYY-MM-DD"
+    """
+    os.chdir(os.getcwd())
+    validate_cwd()
+    validateconfig()
+    makedirs()
+
+    if not eliminate:
+        eliminate = False
+
+    from jessetk.refine import refine
+    r = refine(dna_file, start_date, finish_date, eliminate)
+    r.run(dna_file, start_date, finish_date)
+
+
+@cli.command()
+@click.argument('start_date', required=True, type=str)
+@click.argument('finish_date', required=True, type=str)
+@click.argument('iterations', required=False, type=int)
+@click.argument('width', required=False, type=int)
+@click.option(
+    '--cpu', default=0, show_default=True,
+    help='The number of CPU cores that Jesse is allowed to use. If set to 0, it will use as many as is available on your machine.')
+def randomth(start_date: str, finish_date: str, iterations: int, width: int, cpu: int) -> None:
+    """
+                                random walk backtest w/ threading.
+                                Enter period "YYYY-MM-DD" "YYYY-MM-DD
+                                Number of tests to perform  eg. 40
+                                Sample width in days        eg. 30"
+                                Thread counts to use        eg. 4
+    """
+
+    os.chdir(os.getcwd())
+    validate_cwd()
+    validateconfig()
+    makedirs()
+
+    if not iterations or iterations < 0:
+        iterations = 32
+        print(f'Iterations not provided, falling back to {iterations} iters!')
+    if not width:
+        width = 40
+        print(
+            f'Window width not provided, falling back to {width} days window!')
+
+    from subprocess import Popen, PIPE
+    from timeit import default_timer as timer
+    from multiprocessing import cpu_count
+    from copy import deepcopy
+
+    max_cpu = cpu_count()
+    iters = iterations
+    processes = []
+    commands = []
+    results = []
+    sorted_results = []
+    iters_completed = 0
+
+    from jessetk.randomwalk import RandomWalk
+    random_walk = RandomWalk(start_date, finish_date, 1, width)
+
+    if cpu > cpu_count():
+        raise ValueError(
+            f'Entered cpu cores number is more than available on this machine which is {cpu_count()}')
+    elif cpu == 0:
+        max_cpu = cpu_count()
+    else:
+        max_cpu = cpu
+
+    print('Cpu count:', cpu_count(), 'Used:', max_cpu)
+
+    start = timer()
+    while iters > 0:
+        commands = []
+        for _ in range(max_cpu):
+            if iters > 0:
+                # Create a random period between given period
+                rand_period_start, rand_period_finish = random_walk.make_random_period()
+                commands.append(
+                    f'jesse-tk backtest {rand_period_start} {rand_period_finish}')
+                iters -= 1
+
+        processes = [Popen(cmd, stdout=PIPE) for cmd in commands]
+        # wait for completion
+        for p in processes:
+            p.wait()
+
+            # Get thread's console output
+            (output, err) = p.communicate()
+            iters_completed += 1
+
+            # Map console output to a dict
+            metric = utils.get_metrics3(output.decode('utf-8'))
+
+            if metric not in results:
+                results.append(deepcopy(metric))
+
+            sorted_results = sorted(
+                results, key=lambda x: float(x['serenity']), reverse=True)
+
+            eta_per_iter = (timer() - start) / iters_completed
+            speed = round(width / eta_per_iter, 2)
+            eta = eta_per_iter * (iterations - iters_completed) # Remaining
+            remaining_time = eta_per_iter * iterations          # estimated total time
+            eta_formatted = strftime("%H:%M:%S", gmtime(eta))
+            remaining_formatted = strftime("%H:%M:%S", gmtime(remaining_time))
+
+            clear_console()
+
+            print(
+                f'{iters_completed}/{iterations}\teta: {eta_formatted}/{remaining_formatted} | Speed: {speed} days/sec | {metric["exchange"]} '
+                f'| {metric["symbol"]} | {metric["tf"]} | {repr(metric["dna"])} '
+                f'| Period: {start_date} -> {finish_date} | Sample width: {width} v4')
+
+            metric = {}
+            random_walk.print_tops_formatted(sorted_results)
+
+    utils.create_csv_report(
+        sorted_results, random_walk.report_file_name, random_file_header)
 
 
 @cli.command()
@@ -158,11 +292,12 @@ def random(start_date: str, finish_date: str, iterations: int, width: int) -> No
         exit()
 
     if not iterations or iterations < 0:
-        iterations = 30
-        print('Iterations not provided, falling back to 30 iters!')
+        iterations = 32
+        print(f'Iterations not provided, falling back to {iterations} iters!')
     if not width:
-        width = 30
-        print(f'Window width not provided, falling back to {width} days window!')
+        width = 40
+        print(
+            f'Window width not provided, falling back to {width} days window!')
 
     from jessetk.randomwalk import RandomWalk
     random_walk = RandomWalk(start_date, finish_date, iterations, width)
@@ -170,8 +305,10 @@ def random(start_date: str, finish_date: str, iterations: int, width: int) -> No
     print_initial_msg()
     start = timer()
     results = []
+    sorted_results = []
     for index in range(1, iterations + 1):
-        rand_period_start, rand_period_finish = random_walk.make_random_period()  # Create a random period between given period
+        # Create a random period between given period
+        rand_period_start, rand_period_finish = random_walk.make_random_period()
 
         # Run jesse backtest and grab console output
         console_output = utils.run_test(rand_period_start, rand_period_finish)
@@ -179,21 +316,24 @@ def random(start_date: str, finish_date: str, iterations: int, width: int) -> No
         # Scrape console output and return metrics as a dict
         metric = utils.get_metrics3(console_output)
 
-        # Shared values TODO Make it common for all tools
-        metric['dna'] = random_walk.dna
-        metric['exchange'] = random_walk.exchange
-        metric['symbol'] = random_walk.symbol
-        metric['tf'] = random_walk.timeframe
+        # # Shared values TODO Make it common for all tools
+        # metric['dna'] = random_walk.dna
+        # metric['exchange'] = random_walk.exchange
+        # metric['symbol'] = random_walk.symbol
+        # metric['tf'] = random_walk.timeframe
 
-        # Add test specific values
-        metric['start_date'] = rand_period_start
-        metric['finish_date'] = rand_period_finish
+        # # Add test specific values
+        # metric['start_date'] = rand_period_start
+        # metric['finish_date'] = rand_period_finish
 
         if metric not in results:
             results.append(copy.deepcopy(metric))
+
         # f.write(str(metric) + '\n')  # Logging disabled
         # f.flush()
-        random_walk.sorted_results = sorted(results, key=lambda x: float(x['serenity']), reverse=True)
+        # random_walk.sorted_results = sorted(results, key=lambda x: float(x['serenity']), reverse=True)
+        sorted_results = sorted(
+            results, key=lambda x: float(x['serenity']), reverse=True)
 
         eta = ((timer() - start) / index) * (iterations - index)
         eta_formatted = strftime("%H:%M:%S", gmtime(eta))
@@ -204,9 +344,12 @@ def random(start_date: str, finish_date: str, iterations: int, width: int) -> No
             f'| {random_walk.symbol} | {random_walk.timeframe} | {repr(random_walk.dna)} '
             f'| Period: {start_date} -> {finish_date} | Sample width: {width}')
 
-        random_walk.print_tops_formatted()
+        metric = {}
+        random_walk.print_tops_formatted(sorted_results)
 
-    utils.create_csv_report(random_walk.sorted_results, random_walk.report_file_name, random_file_header)
+    utils.create_csv_report(
+        sorted_results, random_walk.report_file_name, random_file_header)
+
 
 @cli.command()
 @click.argument('dna_file', required=True, type=str)
@@ -235,7 +378,8 @@ def randomsg(dna_file, start_date: str, finish_date: str, iterations: int, width
         print('Iterations not provided, falling back to 30 iters!')
     if not width:
         width = 50
-        print(f'Window width not provided, falling back to {width} days window!')
+        print(
+            f'Window width not provided, falling back to {width} days window!')
 
     from jessetk.randomwalk import RandomWalk
     random_walk = RandomWalk(start_date, finish_date, iterations, width)
@@ -245,9 +389,11 @@ def randomsg(dna_file, start_date: str, finish_date: str, iterations: int, width
     # start = timer()
     results = []
     for _ in range(1, iterations + 1):
-        rand_period_start, rand_period_finish = random_walk.make_random_period()  # Create a random period between given period
+        # Create a random period between given period
+        rand_period_start, rand_period_finish = random_walk.make_random_period()
 
-        r = refine(dna_file, rand_period_start, rand_period_finish, eliminate=True)
+        r = refine(dna_file, rand_period_start,
+                   rand_period_finish, eliminate=True)
         r.run(dna_file, rand_period_start, rand_period_finish)
 
         if len(r.sorted_results) <= 5:
@@ -256,7 +402,6 @@ def randomsg(dna_file, start_date: str, finish_date: str, iterations: int, width
 
         # eta = ((timer() - start) / index) * (iterations - index)
         # eta_formatted = strftime("%H:%M:%S", gmtime(eta))
-
 
 
 # // *
@@ -293,20 +438,6 @@ def score() -> None:
     run()
 
 
-@cli.command()
-def fixcsv() -> None:
-    """
-    z
-    """
-    os.chdir(os.getcwd())
-    validate_cwd()
-
-    from jessetk.fixCsv import run
-    # validateconfig()
-    # makedirs()
-    run()
-
-
 # ///
 @cli.command()
 @click.argument('start_date', required=True, type=str)
@@ -328,6 +459,7 @@ def testpairs(start_date: str, finish_date: str) -> None:
 @cli.command()
 @click.argument('start_date', required=True, type=str)
 @click.argument('finish_date', required=True, type=str)
+@click.argument('hyperparameters', required=False, type=str)
 @click.option('--debug/--no-debug', default=False,
               help='Displays logging messages instead of the progressbar. Used for debugging your strategy.')
 @click.option('--csv/--no-csv', default=False,
@@ -342,7 +474,7 @@ def testpairs(start_date: str, finish_date: str) -> None:
               help="Generates an output that can be copy-and-pasted into tradingview.com's pine-editor too see the trades in their charts.")
 @click.option('--full-reports/--no-full-reports', default=False,
               help="Generates QuantStats' HTML output with metrics reports like Sharpe ratio, Win rate, Volatility, etc., and batch plotting for visualizing performance, drawdowns, rolling statistics, monthly returns, etc.")
-def backtest(start_date: str, finish_date: str, debug: bool, csv: bool, json: bool, fee: bool, chart: bool,
+def backtest(start_date: str, finish_date: str, hyperparameters: str, debug: bool, csv: bool, json: bool, fee: bool, chart: bool,
              tradingview: bool, full_reports: bool) -> None:
     """
     backtest mode. Enter in "YYYY-MM-DD" "YYYY-MM-DD"
@@ -378,29 +510,18 @@ def backtest(start_date: str, finish_date: str, debug: bool, csv: bool, json: bo
             config['env']['exchanges'][e]['fee'] = 0
             get_exchange(e).fee = 0
 
-    # if is_jesse_project:
-    #     inject_local_config()
-    #     inject_local_routes()
-
-    print(router.routes[0].__dict__)
-
-    print('************ BEFORE ****************')
-    print(config['app']['considering_candles'], '/////////////////////////////////////')
-    print(config['app']['trading_symbols'], '/////////////////////////////////////')
-    print('************ AFTER ****************')
-    # config['app']['considering_candles'] = (('Binance Futures', 'MATIC-USDT'),)
-    # config['app']['trading_symbols'] = ("MATIC-USDT",)
-
-    print(config['app']['considering_candles'], '/////////////////////////////////////')
-    print(config['app']['trading_symbols'], '/////////////////////////////////////')
+    # print(router.routes[0].__dict__)
 
     # backtest_mode._initialized_strategies()
-
     backtest_mode.run(start_date, finish_date, chart=chart, tradingview=tradingview, csv=csv,
                       json=json, full_reports=full_reports)
 
-    data = report.portfolio_metrics()
-    print(data)
+    # try:    # Catch error when there's no trades.
+    #     data = report.portfolio_metrics()
+    #     print(data)
+    # except:
+    #     print('No Trades, no metrics!')
+
     db.close_connection()
 
 
